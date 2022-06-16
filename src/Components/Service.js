91145d2ccb004ac617d21f13cb8830a4f1c43321
Ronak Lala <ronaklala2010@gmail.com>
2022-06-16 18:47:32 +0530

    fix

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,47 +1,32 @@
-import React from 'react'
-import './css/service.css';
-import tyre from '../Components/img/tyre_img.png';
+import React from "react";
+import "./css/service.css";
+import tyre from "../Components/img/tyre_img.png";
 const Service = () => {
-<<<<<<< HEAD
   return (
     <>
-      <br />
-      <br />
       <h1 className="heading">Tyres</h1>
       <div class="container1">
         <div class="card1">
           <div class="imgBx">
             <img src={tyre} alt="tyre" />
           </div>
-=======
-    return (
-        <>
-            <h1 className='heading'>Tyres</h1>
-            <div class="container1">
-                <div class="card1">
-                    <div class="imgBx">
-                        <img src={tyre} alt="tyre" />
-                    </div>
->>>>>>> e8e9f0cf76c9f2de4ff790e792fc7ad5dbf4a032
 
-                    <div class="contentBx">
+          <div class="contentBx">
+            <h2>Apollo tyre</h2>
 
-                        <h2>Apollo tyre</h2>
-
-                        <div class="size1">
-                            <h3>size1 :</h3>
-                            <span>7</span>
-                            <span>8</span>
-                            <span>9</span>
-                            <span>10</span>
-                        </div>
-                        <a href="/product-detail">View more</a>
-                    </div>
-
-                </div>
+            <div class="size1">
+              <h3>size1 :</h3>
+              <span>7</span>
+              <span>8</span>
+              <span>9</span>
+              <span>10</span>
             </div>
-        </>
-    )
-}
+            <a href="/product-detail">View more</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Service;
