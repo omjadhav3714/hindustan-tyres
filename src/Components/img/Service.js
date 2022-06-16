a959bd396e7e6c41c35b932886c5b1f2a3502913
Ronak Lala <ronaklala2010@gmail.com>
2022-06-16 17:18:19 +0530

    small fix

--- src/Components/img/Service.js
+++ src/Components/img/Service.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import './service.css';
-import  tyre from  '../img/tyre_img.png';
+import React from "react";
+import "./service.css";
+import tyre from "../img/tyre_img.png";
 const Service = () => {
   return (
     <>
-            <h1 className='heading'>Tyres</h1>
-        <div class="container1">
+      <h1 className="heading">Tyres</h1>
+      <div class="container1">
         <div class="card1">
-            <div class="imgBx">
-                <img src={tyre} alt="tyre"/>
+          <div class="imgBx">
+            <img src={tyre} alt="tyre" />
+          </div>
+
+          <div class="contentBx">
+            <h2>Apollo tyre</h2>
+
+            <div class="size">
+              <h3>Size :</h3>
+              <span>7</span>
+              <span>8</span>
+              <span>9</span>
+              <span>10</span>
             </div>
-
-            <div class="contentBx">
-
-                <h2>Apollo tyre</h2>
-
-                <div class="size">
-                    <h3>Size :</h3>
-                    <span>7</span>
-                    <span>8</span>
-                    <span>9</span>
-                    <span>10</span>
-                </div>
-{/* 
+            {/* 
                 <div class="color">
 
                     <h3>Color :</h3>
@@ -30,13 +29,12 @@ const Service = () => {
                     <span></span>
                     <span></span>
                 </div> */}
-                <a href="#">View more</a>
-            </div>
-
+            <a href="#">View more</a>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Service
+export default Service;
