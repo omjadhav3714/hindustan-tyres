0446d7cf684c230af9fbb978e7b46644ad834333
adil-khatri <ADILAQZMA@GMAIL.COM>
2022-06-20 19:45:16 +0530

    Contactus data posting done

--- src/Components/Contactus.js
+++ src/Components/Contactus.js
@@ -1,10 +1,34 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, { useState } from 'react'
 import '../Components/css/theme.css';
-
 import '../Components/css/maincons.css';
 import '../Components/css/bootstrap.css';
+import { db } from '../firebase';
+
 const Contactus = () => {
+  const [name , setName] = useState("");
+  const [email , setEmail] = useState("");
+  const [message , setMessage] = useState("");
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    db.collection("contact")
+    .add({
+      name: name,
+      email: email,
+      message : message,
+    })
+    .then(() =>{
+      alert("Successfully Submitted");
+    }).catch((error)=>{
+      alert(error.message);
+    });
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  }
+
   return (
     <>
       <main>
@@ -36,15 +60,15 @@ const Contactus = () => {
                 <h2 class="title-section">Drop Us a Line</h2>
                 <div class="divider"></div>
 
-                <form action="#">
+                <form onSubmit={handleSubmit}>
                   <div class="py-2">
-                    <input type="text" class="form-control" placeholder="Full name" />
+                    <input type="text" class="form-control" placeholder="Full name"  value={name} onChange={(e) => setName(e.target.value)} />
                   </div>
                   <div class="py-2">
-                    <input type="text" class="form-control" placeholder="Email" />
+                    <input type="text" class="form-control" placeholder="Email"  value={email} onChange={(e) => setEmail(e.target.value)} />
                   </div>
                   <div class="py-2">
-                    <textarea rows="6" class="form-control" placeholder="Enter message"></textarea>
+                    <textarea rows="6" class="form-control" placeholder="Enter message" value={message} onChange={(e) => setMessage(e.target.value)} ></textarea>
                   </div>
                   <button type="submit" class="btn btn-primary rounded-pill mt-4">Send Message</button>
                 </form>
