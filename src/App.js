32774ecd9db90cff43edbcf79f4e63804ea014d1
Hasnain01-hub <hasnainsayyed833@gmail.com>
2022-06-21 22:08:40 +0530

    Product for home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router";
 import Contactus from "./Components/Contactus";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
+import Static_Detail from "./Components/Static_Detail";
 import About from "./Components/About";
 import Footer from './Components/Footer'
 import ProductDetail from "./Components/Product/ProductDetail";
@@ -17,6 +18,8 @@ function App() {
         <Route exact path="/product-detail/:id" component={ProductDetail} />
         <Route exact path="/contactus" component={Contactus} />
         <Route exact path="/allservice" component={Allservice}/>
+        <Route exact path="/product-static/:id" component={Static_Detail}/>
+        
         <Route exact path="/aboutus" component={About} />
       </Switch>
       
--- src/Components/Service.js
+++ src/Components/Service.js
@@ -2,17 +2,65 @@ import React from "react";
 import "./css/service.css";
 import tyre from "../Components/img/tyre_img.png";
 const Service = () => {
+  const data = [
+    {
+      id: 1,
+      name: "Apollo Tyre",
+      image: tyre,
+      desc: "this is a description",
+      pattern: "Cross",
+    },
+    {
+      id: 2,
+      name: "BridgeStone Tyre",
+      image:
+        "https://www.bridgestone.co.in/content/dam/bridgestone/consumer/bst/apac/In/ProductImages/Optmisedtyreimgs/Potenza/tyres-for-high-speed-vehicles.jpg/_jcr_content/renditions/cq5dam.thumbnail.319.319.png",
+      desc: "this is a description",
+      pattern: "Cross",
+    },
+    {
+      id: 3,
+      name: "CEAT Tyre",
+      image:
+        "	https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5tcjG0pvjrZzqKKoYaNzx7eQ5AsZRPtuvMw&usqp=CAU",
+      desc: "this is a description",
+      pattern: "Cross",
+    },
+    {
+      id: 4,
+      name: "JK Tyre",
+      image: "https://m.media-amazon.com/images/I/81TjxZOFB0L._SX425_.jpg",
+      desc: "this is a description",
+      pattern: "Cross",
+    },
+    {
+      id: 5,
+      name: "Ultramile Tyre",
+      image: "https://trimg.cardekho.com/model/200x200/um-4x4-a-t-bull.jpg",
+      desc: "this is a description",
+      pattern: "Cross",
+    },
+    {
+      id: 6,
+      name: "Yokohama Tyre",
+      image:
+        "https://m.media-amazon.com/images/I/41gr5PWnGUL.jpg",
+      desc: "this is a description",
+      pattern: "Cross",
+    },
+  ];
   return (
     <>
       <h1 className="heading">Tyres</h1>
       <div class="container1">
+        {data.map((item) => (
         <div class="card1">
           <div class="imgBx">
-            <img src={tyre} alt="tyre" />
+            <img src={item.image} alt="tyre" />
           </div>
 
           <div class="contentBx">
-            <h2>Apollo tyre</h2>
+            <h2>{item.name}</h2>
 
             <div class="size1">
               <h3>size1 :</h3>
@@ -21,9 +69,10 @@ const Service = () => {
               <span>9</span>
               <span>10</span>
             </div>
-            <a href="/product-detail">View more</a>
+            <a href={"/product-static/"+item.id}>View more</a>
           </div>
         </div>
+))}
       </div>
     </>
   );
